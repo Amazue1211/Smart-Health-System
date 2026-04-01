741294001812785b5729ef11f16f3c40e68a1f6a
{Amazue1211} <amazuechima7@gmail.com>
2026-04-01 23:50:32 +0100

    22

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -123,7 +123,7 @@ className='mt-[100px] '
             Login
         </button> <br />
          <button type='submit' className='bg-[#3566bb] cursor-[pointer] w-[320px] h-[50px] mt-[20px] rounded-[10px] mt-[10px] text-white font-bold'>
-       Sign up
+       Sign u
         </button>
 </form>
 
