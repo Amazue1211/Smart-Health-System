ccdb6c414a0acc593a402a391a5a598944211cf8
{Amazue1211} <amazuechima7@gmail.com>
2026-04-01 23:04:03 +0100

    18th comit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Notfound from './Pages/Notfound'
+import Notification from './Pages/Notification'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+function App() {
+  
+
+  return (
+    <>
+
+        <Router>
+  <Routes>
+   <Route index element={<Login/>}/>
+   <Route path='notification' element = {<Notification />} />
+     <Route path='login' element = {<Login />} />
+   <Route path='*' element = {<Notfound/>} />
+ </Routes>
+ </Router>
+    </>
+  )
+}
+
+export default App
